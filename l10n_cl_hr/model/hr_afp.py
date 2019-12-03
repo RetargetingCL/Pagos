597b893ea115e8195775c0be0bf0c732784d6ab5
RetargetingCL <54725593+RetargetingCL@users.noreply.github.com>
2019-12-03 11:53:45 -0300

    prueba

--- l10n_cl_hr/model/hr_afp.py
+++ l10n_cl_hr/model/hr_afp.py
@@ -8,6 +8,6 @@ class hr_afp(models.Model):
     codigo = fields.Char('Codigo', required=True)
     name = fields.Char('Nombre', required=True)
     rut = fields.Char('RUT', required=True)
-    rate = fields.Float('Tasa', required=True)
+    rate = fields.Float('Tasa', required=True) 
     sis = fields.Float('Aporte Empresa', required=True)
     independiente = fields.Float('Independientes', required=True)
